77b25566a086fdbf27ad0e112185b6b5afbe0c55
Lucas <lucas.silva@agenciamilagro.com.br>
2014-02-03 09:38:36 -0200

    Fix a bunch of typos

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -76,190 +76,212 @@ module.exports = function (grunt) {
                         '<%%= yeoman.dist %>/*'
                     ]
                 }],
-                server: [
-                    '.tmp'
-                ]
-            }, <% if (cssPre === 'sass') { %>
-            sass: {
+            },
+            server: [
+                '.tmp'
+            ]
+        }, <% if (cssPre === 'sass') { %>
+        sass: {
+            options: {
+                bundleExec: true,
+                debugInfo: false,
+                lineNumbers: false,
+                loadPath: 'app/_bower'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/<%= sassDir %>',
+                    src: '**/*.{scss,sass}',
+                    dest: '.tmp/<%= cssDir %>',
+                    ext: '.css'
+                }]
+            },
+            server: {
                 options: {
-                    bundleExec: true,
-                    debugInfo: false,
-                    lineNumbers: false,
-                    loadPath: 'app/_bower'
+                    debugInfo: true,
+                    lineNumbers: true
                 },
-                dist: {
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.app %>/<%= sassDir %>',
-                        src: '**/*.{scss,sass}',
-                        dest: '.tmp/<%= cssDir %>',
-                        ext: '.css'
-                    }]
-                },
-                server: {
-                    options: {
-                        debugInfo: true,
-                        lineNumbers: true
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.app %>/<%= sassDir %>',
-                        src: '**/*.{scss,sass}',
-                        dest: '.tmp/<%= cssDir %>',
-                        ext: '.css'
-                    }]
-                }
-            }, <% } %><% if (cssPre === 'compass') { %>
-            compass: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/<%= sassDir %>',
+                    src: '**/*.{scss,sass}',
+                    dest: '.tmp/<%= cssDir %>',
+                    ext: '.css'
+                }]
+            }
+        }, <% } %><% if (cssPre === 'compass') { %>
+        compass: {
+            options: {
+                bundleExec: true,
+                sassDir: '<%%= yeoman.app %>/<%= sassDir %>',
+                cssDir: '.tmp/<%%= cssDir %>',
+                imagesDir: '<%%= yeoman.app %>/<%= imgDir %>',
+                javascriptDir: '<%%= yeoman.app %>/<%= jsDir %>',
+                relativeAssets: false,
+                httpImagesPath: '/<%= imgDir %>',
+                httpGeneratedImagesPath: '/<%= imgDir %>/generated',
+                outputStyle: 'expand',
+                raw: 'extensions_dir = "<%%= yeoman.app %>/_bower"\n'
+            },
+            dist: {
                 options: {
-                    bundleExec: true,
-                    sassDir: '<%%= yeoman.app %>/<%= sassDir %>',
-                    cssDir: '.tmp/<%%= cssDir %>',
-                    imagesDir: '<%%= yeoman.app %>/<%= imgDir %>',
-                    javascriptDir: '<%%= yeoman.app %>/<%= jsDir %>',
-                    relativeAssets: false,
-                    httpImagesPath: '/<%= imgDir %>',
-                    httpGeneratedImagesPath: '/<%= imgDir %>/generated',
-                    outputStyle: 'expand',
-                    raw: 'extensions_dir = "<%%= yeoman.app %>/_bower"\n'
-                },
-                dist: {
-                    options: {
-                        generatedImagesDir: '<%%= yeoman.dist %>/<%= imgDir %>/generated'
-                    }
-                },
-                server: {
-                    options: {
-                        debugInfo: true,
-                        generatedImagesDir: '.tmp/<%= imgDir %>/generated'
-                    }
-                }
-            },<% } %><% if (autoPre) { %>
-            autoprefixer: {
-                options: {
-                    browsers: ['last 2 versions']
-                },
-                dist: {
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.dist %>/<%= cssDir %>',
-                        src: '**/*.css',
-                        dest: '<%%= yeoman.dist %>/<%= cssDir %>'
-                    }]
-                },
-                server: {
-                    files: [{
-                        expand: true,
-                        cwd: '.tmp/<%= cssDir %>',
-                        src: '**/*.css',
-                        dest: '.tmp/<%= cssDir %>'
-                    }]
+                    generatedImagesDir: '<%%= yeoman.dist %>/<%= imgDir %>/generated'
                 }
             },
-            useminPrepare: {
+            server: {
                 options: {
+                    debugInfo: true,
+                    generatedImagesDir: '.tmp/<%= imgDir %>/generated'
+                }
+            }
+        },<% } %><% if (autoPre) { %>
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.dist %>/<%= cssDir %>',
+                    src: '**/*.css',
+                    dest: '<%%= yeoman.dist %>/<%= cssDir %>'
+                }]
+            },
+            server: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/<%= cssDir %>',
+                    src: '**/*.css',
+                    dest: '.tmp/<%= cssDir %>'
+                }]
+            }
+        },
+        useminPrepare: {
+            options: {
+                dest: '<%%= yeoman.dist %>'
+            },
+            html: '<%%= yeoman.dist %>/index.html'
+        },
+        usemin: {
+            options: {
+                assetsDir: '<%%= yeoman.dist %>',
+            },
+            html: ['<%%= yeoman.dist %>/**/*.html'],
+            css: ['<%%= yeoman.dist %>/<%= cssDir %>/**/*.css']
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributesQuotes: false,
+                    removeRedundantAttributes: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.dist %>',
+                    src: '**/*.html',
                     dest: '<%%= yeoman.dist %>'
-                },
-                html: '<%%= yeoman.dist %>/index.html'
-            },
-            usemin: {
+                }]
+            }
+        },
+        cssmin: {
+            dist: {
                 options: {
-                    assetsDir: '<%%= yeoman.dist %>',
-                },
-                html: ['<%%= yeoman.dist %>/**/*.html'],
-                css: ['<%%= yeoman.dist %>/<%= cssDir %>/**/*.css']
-            },
-            htmlmin: {
-                dist: {
-                    options: {
-                        collapseWhitespace: true,
-                        collapseBooleanAttributes: true,
-                        removeAttributesQuotes: false,
-                        removeRedundantAttributes: true
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.dist %>',
-                        src: '**/*.html',
-                        dest: '<%%= yeoman.dist %>'
-                    }]
+                    check: 'gzip'
                 }
-            },
-            cssmin: {
-                dist: {
-                    options: {
-                        check: 'gzip'
-                    }
-                }
-            },
-            imagemin: {
-                dist: {
-                    options: {
-                        progressive: true
-                    },
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.dist %>',
-                        src: '**/*.{jpg,jpeg,png}',
-                        dest: '<%%= yeoman.dist %>'
-                    }]
-                }
-            },
-            svgmin: {
-                dist: {
-                    files: [{
-                        expand: true,
-                        cwd: '<%%= yeoman.dist %>',
-                        src: '**/*.svg',
-                        dest: '<%%= yeoman.dist %>'
-                    }]
-                }
-            },
-            copy: {
-                dist: {
-                    files: [{
-                        expand: true,
-                        dot: true,
-                        cwd: '<%%= yeoman.app %>',
-                        dest: '<%%= yeoman.dist %>'
-                    }]
-                }<% if (autoPre) { %>,
-                stageCss: {
-                    files: [{
-                        expand: true,
-                        dot: true,
-                        cwd: '<%%= yeoman.app %>/<%= cssDir %>',
-                        src: '**/*.css',
-                        dest: '.tmp/<%= cssDir %>'
-                    }]
-                }<% } %>
-            },
-            filerev: {
+            }
+        },
+        imagemin: {
+            dist: {
                 options: {
-                    length: 4
+                    progressive: true
                 },
-                dist: {
-                    files: [{
-                        src: [
-                            '<%%= yeoman.dist %>/<%= jsDir %>/**/*.js',
-                            '<%%= yeoman.dist %>/<%= cssDir %>/**/*.css',
-                            '<%%= yeoman.dist %>/<%= imgDir %>/**/*.{gif,jpg,jpeg,png,svg,webp}',
-                            '<%%= yeoman.dist %>/<%= fontsDir %>/**/*.{eot*,otf,svg,ttf,woff}'
-                        ]
-                    }]
-                }
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.dist %>',
+                    src: '**/*.{jpg,jpeg,png}',
+                    dest: '<%%= yeoman.dist %>'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.dist %>',
+                    src: '**/*.svg',
+                    dest: '<%%= yeoman.dist %>'
+                }]
+            }
+        },
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%%= yeoman.app %>',
+                    dest: '<%%= yeoman.dist %>'
+                }]
+            }<% if (autoPre) { %>,
+            stageCss: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%%= yeoman.app %>/<%= cssDir %>',
+                    src: '**/*.css',
+                    dest: '.tmp/<%= cssDir %>'
+                }]
+            }<% } %>
+        },
+        filerev: {
+            options: {
+                length: 4
             },
-            jshint: {
-                options: {
-                    jshint: '.jshintrc',
-                    reporter: require('jshint-stylish')
-                },
-                all: [
-                    'Gruntfile.js',
-                    '<%%= yeoman.app %>/<%= jsDir %>/**/*.js',
-                    // 'test/spec/**/*.js'
+            dist: {
+                files: [{
+                    src: [
+                        '<%%= yeoman.dist %>/<%= jsDir %>/**/*.js',
+                        '<%%= yeoman.dist %>/<%= cssDir %>/**/*.css',
+                        '<%%= yeoman.dist %>/<%= imgDir %>/**/*.{gif,jpg,jpeg,png,svg,webp}',
+                        '<%%= yeoman.dist %>/<%= fontsDir %>/**/*.{eot*,otf,svg,ttf,woff}'
+                    ]
+                }]
+            }
+        },
+        jshint: {
+            options: {
+                jshint: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js',
+                '<%%= yeoman.app %>/<%= jsDir %>/**/*.js',
+                // 'test/spec/**/*.js'
+            ]
+        },
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc'
+            },
+            check: {
+                src: [
+                    '<%%= yeoman.app %>/<%= cssDir %>/**/*.css'
                 ]
-            },
+            }
+        },
+        concurrent: {
+            server: [<% if (cssPre === 'sass') { %>
+                'sass:server',<% } %><% if (cssPre === 'compass') { %>
+                'compass:server',<% } %><% if (autoPre) { %>
+                'copy:stageCss',<% } %>
+            ],
+            dist: [<% if (cssPre === 'sass') { %>
+                'sass:dist',<% } %><% if (cssPre === 'compass') { %>
+                'compass:dist',<% } %>
+                'copy:dist'
+            ]
         }
     });
 
